3d2d9e443ff5784f2355be70aa32c85640d44f62
Ben Stoica <bstoica@uwm.com>
2022-01-31 14:03:29 -0500

    component setup and data fetching

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, setFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className='App'>
       <h1 className='text-center ubuntu my-4'>
         <span className='text-primary'>Wiki</span>
       </h1>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-3'>
+            <Filters />
+          </div>
+          <div className='col-8'>
+            <div className='row'>
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
