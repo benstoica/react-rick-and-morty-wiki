bfa9f7701db89a0b8325201ca9163f31a89ce706
Ben Stoica <bstoica@uwm.com>
2022-02-01 11:09:12 -0500

    using react-paginate for pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,23 +1,26 @@
 import React from "react";
+import ReactPaginate from "react-paginate";
+import styles from "./Pagination.module.scss";
 
-const Pagination = ({ pageNumber, setPageNumber }) => {
-  let prev = () => {
-    if (pageNumber === 1) return;
-    setPageNumber((page) => page - 1);
-  };
-  let next = () => {
-    setPageNumber((page) => page + 1);
-  };
-
+const Pagination = ({ info, pageNumber, setPageNumber }) => {
   return (
-    <div className='container d-flex justify-content-center gap-5 my-5'>
-      <button onClick={prev} className='btn btn-primary'>
-        Prev
-      </button>
-      <button onClick={next} className='btn btn-primary'>
-        Next
-      </button>
-    </div>
+    <ReactPaginate
+      className='pagination justify-content-center gap-4 my-4'
+      nextLabel='Next'
+      nextClassName='btn btn-primary'
+      nextLinkClassName={`${styles.button}`}
+      previousLabel='Prev'
+      previousClassName='btn btn-primary'
+      previousLinkClassName={`${styles.button}`}
+      pageClassName='page-item'
+      pageLinkClassName='page-link'
+      activeClassName='active'
+      pageCount={info?.pages}
+      onPageChange={(data) => {
+        setPageNumber(data.selected + 1);
+      }}
+      forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+    />
   );
 };
 
